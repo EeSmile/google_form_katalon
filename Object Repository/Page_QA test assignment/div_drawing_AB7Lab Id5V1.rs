b6d6283695c9923c3d65d481318b4881ba88fe0b
Ismayil Taghiyev <ta.ismail@mail.ru>
2022-11-16 19:11:50 +0400

    Add files via upload

---
+++ Object Repository/Page_QA test assignment/div_drawing_AB7Lab Id5V1.rs
@@ -0,0 +1,91 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_drawing_AB7Lab Id5V1</name>
+   <tag></tag>
+   <elementGuidId>6a0fd85f-f1e5-4580-a4b7-41a512a01b11</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='i16']/div[3]/div</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>#i16 > div.vd3tt > div.AB7Lab.Id5V1</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>ee079c4f-dc07-45af-aeec-bb25beacb373</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>AB7Lab Id5V1</value>
+      <webElementGuid>c8de92f7-a89e-4013-8e5c-5f366503f50b</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;i16&quot;)/div[@class=&quot;vd3tt&quot;]/div[@class=&quot;AB7Lab Id5V1&quot;]</value>
+      <webElementGuid>f2d52121-139d-463a-9a02-bcdaf82edbaf</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='i16']/div[3]/div</value>
+      <webElementGuid>35fbec85-cbe7-4c48-bfb1-55818cdbda71</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='drawing'])[1]/following::div[8]</value>
+      <webElementGuid>9b7b83e7-7b47-4295-bfb9-7ed14cf726aa</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='*'])[3]/following::div[24]</value>
+      <webElementGuid>cf4b94ec-d22c-4982-a53f-f4e27db9a9db</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='playing music'])[1]/preceding::div[2]</value>
+      <webElementGuid>c7c73b59-1941-410c-ae9d-9997620eda91</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='a new language'])[1]/preceding::div[11]</value>
+      <webElementGuid>23a214be-efc5-40a3-ab03-82ab5f208e56</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/label/div/div/div/div[3]/div</value>
+      <webElementGuid>c0a5858d-1e8a-46bb-a859-59f57b3623a7</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
